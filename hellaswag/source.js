6b57ca6a9948f5250d24aa282de31a73743d80e3
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-05-25 22:38:36 -0400

    Hellaswag progress

---
+++ hellaswag/source.js
@@ -0,0 +1,29 @@
+const questionLabel = document.getElementById("question-label")
+const idLabel = document.getElementById("id-label")
+const descriptionLabel = document.getElementById("description-label")
+const answerChoiceContainers = Array.from(document.querySelectorAll(".choice"))
+const answerChoices = Array.from(document.querySelectorAll(".choice > p"))
+
+let correctAnswer = 0
+
+for (let i = 0; i < 4; i++) {
+	answerChoiceContainers[i].addEventListener("click", function() { validateAnswer(i) })
+}
+
+function prepareQuestion(scene) {
+	questionLabel.innerText = scene.activity_label
+	idLabel.innerText = "#" + scene.ind
+	descriptionLabel.innerText = scene.ctx
+	for (let i = 0; i < 4; i++) {
+		answerChoices[i].innerText = scene.endings[i]
+	}
+	correctAnswer = scene.label
+}
+
+function validateAnswer(answer) {
+	if (answer == correctAnswer) {
+		prepareQuestion(scenes[Math.floor(Math.random() * scenes.length)])
+	}
+}
+
+prepareQuestion(scenes[Math.floor(Math.random() * scenes.length)])
