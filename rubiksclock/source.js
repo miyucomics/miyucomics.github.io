f9a4a4c94a263b2921c326db702737c4825df1bf
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-07-27 17:39:33 -0400

    General shape done

--- rubiksclock/source.js
+++ rubiksclock/source.js
@@ -2,7 +2,8 @@ import * as THREE from "three"
 import { TrackballControls } from "three/addons/controls/TrackballControls.js"
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.y = 10
 camera.position.z = 15
 
 const renderer = new THREE.WebGLRenderer()
@@ -12,7 +13,11 @@ renderer.setAnimationLoop(animate)
 document.body.appendChild(renderer.domElement)
 
 const controls = new TrackballControls(camera, renderer.domElement)
-controls.rotateSpeed = 25
+controls.rotateSpeed = 5
+controls.noPan = true
+
+scene.add(new THREE.AmbientLight(0xffffff))
+scene.add(new THREE.AmbientLight(0xffffff))
 
 function addCylinder(x, y, z, radius, height, color) {
     const geometry = new THREE.CylinderGeometry(radius, radius, height, 128)
@@ -28,18 +33,51 @@ function addLight (x, y, z) {
     scene.add(light)
 }
 
-addLight(10, 0, 0)
-addLight(-10, 0, 0)
 addLight(0, 10, 0)
 addLight(0, -10, 0)
-addLight(0, 0, 10)
-addLight(0, 0, -10)
+addLight(7.5, 7.5, 0)
+addLight(-7.5, -7.5, 0)
 
 const bodyRadius = 5
-const bodyThickness = 0.4
+const bodyThickness = 0.75
+addCylinder(0, bodyThickness / 4, 0, bodyRadius, bodyThickness / 2, 0xf9e2af)
+addCylinder(0, -bodyThickness / 4, 0, bodyRadius, bodyThickness / 2, 0x89b4fa)
 
-addCylinder(0, bodyThickness / 4, 0, bodyRadius, bodyThickness / 2, 0xcdd6f4)
-addCylinder(0, -bodyThickness / 4, 0, bodyRadius, bodyThickness / 2, 0x1e1e2e)
+const pinRadius = 0.2
+const pinDistance = 1.5
+const pinHeight = 2
+class Pin {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+        const geometry = new THREE.CylinderGeometry(pinRadius, pinRadius, pinHeight, 128)
+        const material = new THREE.MeshStandardMaterial({ color: 0xa6e3a1 })
+        this.mesh = new THREE.Mesh(geometry, material)
+        this.mesh.position.set(x, 0, y)
+        scene.add(this.mesh)
+    }
+}
+new Pin(-pinDistance, pinDistance) 
+new Pin(-pinDistance, -pinDistance) 
+new Pin(pinDistance, -pinDistance) 
+new Pin(pinDistance, pinDistance) 
+
+const clockRadius = 1
+const clockHeight = 1
+const clockSpacing = 2.5
+addCylinder(-clockSpacing, 0, -clockSpacing, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(0, 0, -clockSpacing, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(clockSpacing, 0, -clockSpacing, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(-clockSpacing, 0, 0, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(0, 0, 0, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(clockSpacing, 0, 0, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(-clockSpacing, 0, clockSpacing, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(0, 0, clockSpacing, clockRadius, clockHeight, 0xcba6f7)
+addCylinder(clockSpacing, 0, clockSpacing, clockRadius, clockHeight, 0xcba6f7)
+
+const dialPins = {}
+const pinStates = {}
+const pinChildren = {}
 
 function animate() {
 	controls.update()
