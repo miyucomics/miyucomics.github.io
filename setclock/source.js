5f04f88c772b811409423a213fae53a10b1d434c
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-03-06 17:53:16 -0500

    Mengenlehruhr improvements

--- setclock/source.js
+++ setclock/source.js
@@ -10,23 +10,25 @@ if (parameters.get("active") != null) {
     root.style.setProperty("--active-color", parameters.get("active"))
 }
 
-const sun = document.getElementById("sun")
 const heavenly_hours = Array.from(document.getElementsByClassName("heavenly-hours"))
 const heavenly_minutes = Array.from(document.getElementsByClassName("heavenly-minutes"))
+const heavenly_seconds = Array.from(document.getElementsByClassName("heavenly-seconds"))
 const earthly_hours = Array.from(document.getElementsByClassName("earthly-hours"))
 const earthly_minutes = Array.from(document.getElementsByClassName("earthly-minutes"))
+const earthly_seconds = Array.from(document.getElementsByClassName("earthly-seconds"))
 
 const animate = () => {
     const date = new Date()
-    sun.classList.toggle("active", date.getSeconds() % 2 == 0)
-    const heavenly_hour_count = Math.floor(date.getHours() / 5)
+
+    const heavenly_hour_count = Math.floor(date.getHours() / 7)
     for (let i = 0; i < heavenly_hours.length; i++) {
         heavenly_hours[i].classList.toggle("active", i < heavenly_hour_count)
     }
-    const earthly_hour_count = Math.floor(date.getHours() % 5)
+    const earthly_hour_count = Math.floor(date.getHours() % 7)
     for (let i = 0; i < earthly_hours.length; i++) {
         earthly_hours[i].classList.toggle("active", i < earthly_hour_count)
     }
+
     const heavenly_minutes_count = Math.floor(date.getMinutes() / 5)
     for (let i = 0; i < heavenly_minutes.length; i++) {
         heavenly_minutes[i].classList.toggle("active", i < heavenly_minutes_count)
@@ -35,6 +37,16 @@ const animate = () => {
     for (let i = 0; i < earthly_minutes.length; i++) {
         earthly_minutes[i].classList.toggle("active", i < earthly_minutes_count)
     }
+
+    const heavenly_seconds_count = Math.floor(date.getSeconds() / 5)
+    for (let i = 0; i < heavenly_seconds.length; i++) {
+        heavenly_seconds[i].classList.toggle("active", i < heavenly_seconds_count)
+    }
+    const earthly_seconds_count = Math.floor(date.getSeconds() % 5)
+    for (let i = 0; i < earthly_seconds.length; i++) {
+        earthly_seconds[i].classList.toggle("active", i < earthly_seconds_count)
+    }
+
     requestAnimationFrame(animate)
 }
 
